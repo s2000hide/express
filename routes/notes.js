0b566806dc86cbf80c2590aeb68e75dfa75a7f34
s2000hide <2101140507py@cyber-u.ac.jp>
2025-06-08 14:45:25 +0900

    add mongodb access

--- routes/notes.js
+++ routes/notes.js
@@ -1,21 +1,21 @@
-varexpress= require('express');
-varrouter= express.Router();
+var express= require('express');
+var router= express.Router();
 
 
 
 // 接続情報を設定
 const{ MongoClient} = require("mongodb");
 consturi= "mongodb+srv://2101140507py:<db_password>@cluster0.qipzhju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-constclient= newMongoClient(uri);
+const client= new MongoClient(uri);
 router.get('/', async(req, res) =>{
 
 // データベース、コレクションを指定
-constdatabase= client.db('notes');
-constnotes= database.collection('notes');
+const database= client.db('notes');
+const notes= database.collection('notes');
 
 // idが１のドキュメントを取得
-constquery= { id:2};
-constnote= awaitnotes.findOne(query);
+const query= { id:2};
+const note= awaitnotes.findOne(query);
 
 res.json(note);
 })
