33e2ff487b8de3c2db8742e0de8e8c6f0332fbde
s2000hide <2101140507py@cyber-u.ac.jp>
2025-06-08 16:39:16 +0900

    add catapi

--- routes/notes.js
+++ routes/notes.js
@@ -1,21 +1,21 @@
 var express= require('express');
-var router= express.Router();
+var router = express.Router();
 // 修正
 
 
 // 接続情報を設定
 const{ MongoClient} = require("mongodb");
-const uri= "mongodb+srv://2101140507py:<db_password>@cluster0.qipzhju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const client= new MongoClient(uri);
+const uri = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"; // MongoDBの接続URIを指定
+const client = new MongoClient(uri);
 router.get('/', async(req, res) =>{
 
 // データベース、コレクションを指定
-const database= client.db('notes');
-const notes= database.collection('notes');
+const database = client.db('notes');
+const notes = database.collection('notes');
 
 // idが１のドキュメントを取得
-const query= { id:2};
-const note= await notes.findOne(query);
+const query = { id:2};
+const note = await notes.findOne(query);
 
 res.json(note);
 })
