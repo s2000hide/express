b6b4128d366fe96c0797aa3653d4033e4a2f50fd
s2000hide <2101140507py@cyber-u.ac.jp>
2025-06-08 15:26:37 +0900

    add mongodb access3

--- routes/notes.js
+++ routes/notes.js
@@ -5,7 +5,7 @@ var router= express.Router();
 
 // 接続情報を設定
 const{ MongoClient} = require("mongodb");
-consturi= "mongodb+srv://2101140507py:<db_password>@cluster0.qipzhju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri= "mongodb+srv://2101140507py:<db_password>@cluster0.qipzhju.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 const client= new MongoClient(uri);
 router.get('/', async(req, res) =>{
 
